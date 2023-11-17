cbbb71cd9e9d0fb91a0e8fcd17ec546e893be260
fzramos <64625115+fzramos@users.noreply.github.com>
2023-11-16 18:55:50 -0600

    Adding auth middleware now that the user registration route is done. Now I can protect routes that only users with valid auth credentails can access. Jest unit testing of middleware function also complete

---
+++ server/middleware/auth.js
@@ -0,0 +1,22 @@
+import jwt from 'jsonwebtoken'
+import config from 'config'
+
+module.exports = function(req, res, next) {
+    const token = req.header('x-auth-token')
+    // React client will not use x-auth-token since storing that in local storage would be a security risk
+    // React client will use HTTPS only cookie for authentication
+    // if no x-auth-token is in header, then we'll check if request has HTTPS only cookie
+    if (!token) token = req.cookies.token
+    if (!token) return res.status(401).send('Access denied. No token provided.')
+
+    try {
+        const decoded = jwt.verify(token, process.env.WFJ_jwtPrivateKey)
+        // adding decoded user details to the request, which will be used
+        // by auth protected routes
+        req.user = decoded
+
+        next()
+    } catch (err) {
+        res.status(400).send('Invalid token provided')
+    }
+}
