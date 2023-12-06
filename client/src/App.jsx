d5f04cec0401fb667b5d610cef71260141cc018d
Fred Ramos <fzramos6@gmail.com>
2023-12-06 01:15:52 -0600

    Modified vite configuration to connect React frontend with backend. Created a button to verify the connection

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,8 @@ import React, { useState } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import './App.css';
 import { Icon } from 'leaflet';
+import axios from 'axios';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
@@ -12,6 +14,21 @@ function App() {
       <div className="card">
         <p>🥐🍕🍜</p>{' '}
       </div>
+      {/* test Frontend to Backend connection */}
+      <Button
+        variant="primary"
+        onClick={async () => {
+          try {
+            const res = await axios.post('/api/user/register', {});
+            console.log(res.status);
+            console.log(res.text);
+          } catch (err) {
+            console.log(err.message);
+          }
+        }}
+      >
+        Sign In
+      </Button>
       {/* <WorldMap></WorldMap> */}
       <WorldHeatMap />
     </>
